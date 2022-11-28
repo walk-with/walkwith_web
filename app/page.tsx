e53ee66fab48a85c16c48dfd3fb358b49858d999
59inu <59inu.dev@gmail.com>
2022-11-29 01:02:59 +0900

    feat(main): add Map

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Script from "next/script";
+import { Map } from "../components/main/Map";
 import { PartyList } from "../components/main/PartyList";
 import "../styles/globals.css";
 
 export default function Main() {
   return (
-    <div>
-      <div>지도</div>
+    <div className="flex flex-col gap-6">
+      <Map />
       <PartyList />
     </div>
   );
---
+++ components/main/Map.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+export const Map = () => {
+  const [myPos, setMyPos] = useState<{ lat: number; lng: number }>();
+  const mapRef = useRef();
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(({ coords }) =>
+      setMyPos({ lat: coords.latitude, lng: coords.longitude })
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!myPos) {
+      return;
+    }
+    mapRef.current = new window.naver.maps.Map("map", {
+      center: new window.naver.maps.LatLng(myPos.lat, myPos.lng),
+      zoomControl: true,
+    });
+  }, [myPos]);
+
+  if (!myPos)
+    return (
+      <div className="w-screen h-60 flex justify-center items-center">
+        LOADING
+      </div>
+    );
+
+  return <div id="map" className="w-screen h-60" />;
+};
