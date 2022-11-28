588fb68ff12dc642301ecddd4a013b2af2627685
59inu <59inu.dev@gmail.com>
2022-11-28 23:50:18 +0900

    feat(api): add /api/parties

---
+++ pages/api/parties.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Party } from "../../types";
+
+const date = new Date().toISOString();
+export default function handler(_: NextApiRequest, res: NextApiResponse) {
+  const mockPartyList: Party[] = Array.from(Array(10)).map(
+    (_, i) =>
+      ({
+        id: i.toString(),
+        name: `테스트 산책 모임 ${i}`,
+        tags: ["tag1", "tag2", "tag3"],
+        startAt: date,
+        srcUrl: "",
+        host: { name: "오구" },
+      } as Party)
+  );
+  res.status(200).json(mockPartyList);
+}
