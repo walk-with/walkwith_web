f444a511d631f53292b7c9abcc63df3b0c9e6cbd
59inu <59inu.dev@gmail.com>
2022-11-30 15:43:05 +0900

    feat(main): add a bounds change event listner to Map

--- src/components/main/Map.tsx
+++ src/components/main/Map.tsx
@@ -1,25 +1,52 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
+import { throttle } from "../../utils/throttle";
 
-export const Map = () => {
-  const [myPos, setMyPos] = useState<{ lat: number; lng: number }>();
+// TODO : 네이버 맵 관련 타이핑 보완, LOADING 보완
+interface MapProps {
+  onChangeBounds: (map: any, bounds: any) => void;
+}
+
+export const Map: FC<MapProps> = ({ onChangeBounds }) => {
   const mapRef = useRef();
 
   useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(({ coords }) =>
-      setMyPos({ lat: coords.latitude, lng: coords.longitude })
-    );
-  }, []);
+    const initializeMap = (latitude: number, longitude: number) => {
+      mapRef.current = new window.naver.maps.Map("map", {
+        center: new window.naver.maps.LatLng(latitude, longitude),
+        zoomControl: false,
+      });
 
-  useEffect(() => {
-    if (!myPos) {
-      return;
-    }
-    mapRef.current = new window.naver.maps.Map("map", {
-      center: new window.naver.maps.LatLng(myPos.lat, myPos.lng),
-      zoomControl: true,
-    });
-  }, [myPos]);
+      if (typeof onChangeBounds === "function") {
+        const listener = throttle(function (bounds: any) {
+          console.log({ CurrentBounds: bounds });
+          onChangeBounds(mapRef.current, bounds);
+        }, 500);
+
+        window.naver.maps.Event.addListener(
+          mapRef.current,
+          "bounds_changed",
+          listener
+        );
+      }
+    };
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords || {};
+        initializeMap(latitude, longitude);
+      },
+      (error) => {
+        const SEOUL_CITYHALL_POSITION: [number, number] = [37.33, 126.58];
+
+        if (error.code == error.PERMISSION_DENIED) {
+          console.log("Geolocation: Permission Denied!");
+        }
+
+        initializeMap.apply(null, SEOUL_CITYHALL_POSITION);
+      }
+    );
+  }, [onChangeBounds]);
 
   //   if (!myPos)
   //     return (
