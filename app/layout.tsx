2e327f4dcc41820e454a7dc1add452cec1ae9a57
59inu <59inu.dev@gmail.com>
2022-12-12 17:07:00 +0900

    feat: set dayjs global locale as 'ko'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
 import { ReactNode } from "react";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "../src/react-query/queryClient";
 import "../styles/globals.css";
+
+dayjs.locale("ko");
 
 interface MainLayoutProps {
   children: ReactNode;
