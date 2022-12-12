949db6976d2c130a8e6cbfe28db4cd8a9b551a07
59inu <59inu.dev@gmail.com>
2022-12-12 16:01:08 +0900

    refactor(main): limit map initialization attempts

--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -12,26 +12,41 @@ export const useMap = (mapConfig: MapConfig) => {
   const initializeMap = (latitude: number, longitude: number) => {
     if (mapRef.current) return;
 
-    mapRef.current = new window.naver.maps.Map(mapConfig.targetElementId, {
-      center: new window.naver.maps.LatLng(latitude, longitude),
-      zoomControl: false,
-    });
+    const MAX_INIT_ATTEMPTS_CNT = 10;
+    let attemptedCnt = 0;
 
-    // add map init event listener
-    if (typeof mapConfig.onLoad === "function") {
-      window.naver.maps.Event.once(mapRef.current, "init", () => {
-        mapConfig.onLoad!(mapRef.current.bounds);
+    const process = () => {
+      if (!window.naver) {
+        if (attemptedCnt >= MAX_INIT_ATTEMPTS_CNT) return;
+
+        attemptedCnt++;
+        setTimeout(process, 200);
+        return;
+      }
+
+      mapRef.current = new window.naver.maps.Map(mapConfig.targetElementId, {
+        center: new window.naver.maps.LatLng(latitude, longitude),
+        zoomControl: false,
       });
-    }
 
-    // add bounds changed event listener
-    if (typeof mapConfig.onChangeBounds === "function") {
-      window.naver.maps.Event.addListener(
-        mapRef.current,
-        "bounds_changed",
-        () => mapConfig.onChangeBounds!(mapRef.current.bounds)
-      );
-    }
+      // add map init event listener
+      if (typeof mapConfig.onLoad === "function") {
+        window.naver.maps.Event.once(mapRef.current, "init", () => {
+          mapConfig.onLoad!(mapRef.current.bounds);
+        });
+      }
+
+      // add bounds changed event listener
+      if (typeof mapConfig.onChangeBounds === "function") {
+        window.naver.maps.Event.addListener(
+          mapRef.current,
+          "bounds_changed",
+          () => mapConfig.onChangeBounds!(mapRef.current.bounds)
+        );
+      }
+    };
+
+    process();
   };
 
   const clearMap = useCallback(() => {
